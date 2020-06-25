19385435ad048e2b5656bb314fd3cd32019f80f4
Sam Saccone <samccone@gmail.com>
2020-06-25 14:07:55 -0700

    Add CSS treeshaking to build step

--- build_tools/go.mod
+++ build_tools/go.mod
@@ -3,8 +3,11 @@ module samccone/me
 go 1.14
 
 require (
+	github.com/ericchiang/css v0.0.0-20171210184639-f08e94f04ef6
+	github.com/samccone/css v0.0.0-20200625202919-8a54e79c4a49
 	github.com/tdewolff/minify v2.3.6+incompatible
 	github.com/tdewolff/parse v2.3.4+incompatible // indirect
+	github.com/tdewolff/parse/v2 v2.4.3
 	github.com/tdewolff/test v1.0.6 // indirect
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9
 )
--- build_tools/main.go
+++ build_tools/main.go
@@ -10,18 +10,117 @@ import (
 	"path/filepath"
 	"strings"
 
+	selcss "github.com/samccone/css"
 	"github.com/tdewolff/minify"
 	"github.com/tdewolff/minify/css"
 	mhtml "github.com/tdewolff/minify/html"
+	pcss "github.com/tdewolff/parse/v2/css"
 	"golang.org/x/net/html"
 )
 
-func externalStylesheet(n *html.Node) (err error, href string, stylesheet bool) {
+func selectorMatches(sel string, root *html.Node) (bool, error) {
+	s, err := selcss.Compile(sel)
+	if err != nil {
+		return false, fmt.Errorf("selcss.Compile: %s %w", sel, err)
+	}
+	return len(s.Select(root)) > 0, nil
+}
+
+// Partially lifted from @andybons
+// https://gist.github.com/andybons/c074c2811f70db5fc3d3f89fbc69f81f
+
+func shakeCSS(r io.Reader, doc *html.Node) (out bytes.Buffer, err error) {
+	p := pcss.NewParser(r, false)
+	var buf bytes.Buffer
+	activeRules := 0
+
+	for {
+		gtype, ttype, data := p.Next()
+		_, _ = ttype, data
+		if err := p.Err(); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return buf, err
+		}
+		switch gtype {
+		case pcss.BeginRulesetGrammar,
+			pcss.QualifiedRuleGrammar:
+			var sb strings.Builder
+			for _, v := range p.Values() {
+				sb.Write(v.Data)
+			}
+
+			s := sb.String()
+			// These selectors are not supported.
+			s = strings.TrimSuffix(s, ":link")
+			s = strings.TrimSuffix(s, ":hover")
+			s = strings.TrimSuffix(s, ":active")
+			s = strings.TrimSuffix(s, ":visited")
+
+			matches, err := selectorMatches(s, doc)
+			if err != nil {
+				return buf, fmt.Errorf("selcss.Compile: %w", err)
+			}
+			if matches {
+				activeRules++
+				buf.WriteString(sb.String())
+				if gtype == pcss.QualifiedRuleGrammar {
+					buf.WriteString(",\n")
+				} else if gtype == pcss.BeginRulesetGrammar {
+					buf.WriteString(" {\n")
+				}
+			} else {
+				fmt.Printf("Unused selector %q in CSS\n", s)
+				if gtype == pcss.BeginRulesetGrammar {
+					buf.WriteString(" {\n")
+				}
+			}
+		case pcss.BeginAtRuleGrammar:
+			buf.Write(data)
+			for _, v := range p.Values() {
+				buf.Write(v.Data)
+			}
+			buf.WriteString(" {\n")
+		case pcss.DeclarationGrammar:
+			if activeRules == 0 {
+				// fmt.Println("No active rules to apply")
+				continue
+			}
+			buf.Write(data)
+			buf.WriteString(": ")
+			for _, v := range p.Values() {
+				buf.Write(v.Data)
+			}
+			buf.WriteString(";\n")
+		case pcss.EndRulesetGrammar:
+			if activeRules == 0 {
+				// fmt.Println("No active rules to apply")
+				continue
+			}
+			buf.Write(data)
+			buf.WriteByte('\n')
+			activeRules = 0
+		case pcss.EndAtRuleGrammar:
+			buf.Write(data)
+			buf.WriteByte('\n')
+		case pcss.CommentGrammar:
+			continue
+
+		default:
+			return buf, fmt.Errorf("unsupported grammar: %v", gtype)
+		}
+	}
+
+	return buf, err
+}
+
+func externalStylesheet(n *html.Node) (href string, stylesheet bool, err error) {
 	href = ""
 	stylesheet = false
 
 	if n.Data != "link" {
-		return nil, href, stylesheet
+		return href, stylesheet, nil
 	}
 
 	for _, a := range n.Attr {
@@ -34,12 +133,12 @@ func externalStylesheet(n *html.Node) (err error, href string, stylesheet bool) 
 		}
 	}
 
-	return nil, href, stylesheet
+	return href, stylesheet, nil
 }
 
-func inlineCss(n *html.Node, rootFile string) {
-	if n.Type == html.ElementNode {
-		err, href, stylesheet := externalStylesheet(n)
+func inlineCSS(root *html.Node, cursor *html.Node, rootFile string) {
+	if cursor.Type == html.ElementNode {
+		href, stylesheet, err := externalStylesheet(cursor)
 		if err != nil {
 			log.Fatalf("unable to determine if stylesheet %v", err)
 		}
@@ -53,11 +152,15 @@ func inlineCss(n *html.Node, rootFile string) {
 				log.Fatalf("Unable to open file %v", err)
 			}
 
-			parent := n.Parent
+			parent := cursor.Parent
 			// Remove the current node
-			parent.RemoveChild(n)
+			parent.RemoveChild(cursor)
 			var buf bytes.Buffer
-			err = m.Minify("text/css", &buf, reader)
+			minified, err := shakeCSS(reader, root)
+			if err != nil {
+				log.Fatalf("Error shaking CSS %v", err)
+			}
+			err = m.Minify("text/css", &buf, strings.NewReader(minified.String()))
 			if err != nil {
 				log.Fatalf("Unable to minify css %v", err)
 			}
@@ -69,12 +172,12 @@ func inlineCss(n *html.Node, rootFile string) {
 			parent.AppendChild(styleNode[0])
 		}
 	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		inlineCss(c, rootFile)
+	for c := cursor.FirstChild; c != nil; c = c.NextSibling {
+		inlineCSS(root, c, rootFile)
 	}
 }
 
-func minifyHtml(root *html.Node, w io.Writer) {
+func minifyHTML(root *html.Node, w io.Writer) {
 	m := minify.New()
 	pr, pw := io.Pipe()
 	m.AddFunc("text/html", mhtml.Minify)
@@ -110,8 +213,8 @@ func main() {
 	}
 	pr, pw := io.Pipe()
 	go func() {
-		inlineCss(z, filepath)
-		minifyHtml(z, pw)
+		inlineCSS(z, z, filepath)
+		minifyHTML(z, pw)
 		defer pw.Close()
 	}()
 
